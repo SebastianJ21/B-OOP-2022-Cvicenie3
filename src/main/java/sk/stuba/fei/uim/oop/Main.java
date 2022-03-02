cc2c04d1a7c290da0346b0ac42f8c90ac3b06b39
Sebastian <xjakabcin@stuba.sk>
2022-03-02 18:20:34 +0100

    - created array for students - created function to initialize the array with 5 custom students

--- src/main/java/sk/stuba/fei/uim/oop/Main.java
+++ src/main/java/sk/stuba/fei/uim/oop/Main.java
@@ -48,11 +48,34 @@ class Student{
 
 public class Main {
     public static void main(String[] args) {
+
+        //Uloha 1:
         Student sebike = new Student(10,"Sebi","Jakabcin",21);
         System.out.println(sebike.getAge());
         sebike.setName("Sebastian");
         System.out.println(sebike.getName());
 
+        //Uloha 2:
+        Student[] poleStudentov = new Student[6];
+        Main.create5Students(poleStudentov);
+        poleStudentov[5] = sebike;
+        int a =2;
+
+
+    }
+
+    public static void create5Students(Student[] arr){
+        Student one = new Student(1,"Juraj","Andrasi",21);
+        Student two = new Student(2,"Simona","Jancekova",19);
+        Student three = new Student(3,"Tomas","Krason",25);
+        Student four = new Student(4,"Andrej","Nejedlik",24);
+        Student five = new Student(5,"Alexandra","Holikova",18);
+
+        arr[0] = one;
+        arr[1]= two;
+        arr[2] = three;
+        arr[3] = four;
+        arr[4] = five;
 
     }
 
