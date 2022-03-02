cd558a6f9c9e9168116b597787bfe8ecfb9e833b
Sebastian <xjakabcin@stuba.sk>
2022-03-02 18:11:08 +0100

    Created class Student -attributes are private -created setters and getters

--- src/main/java/sk/stuba/fei/uim/oop/Main.java
+++ src/main/java/sk/stuba/fei/uim/oop/Main.java
@@ -1,7 +1,59 @@
 package sk.stuba.fei.uim.oop;
 
+class Student{
+    private int  ID;
+    private String name;
+    private String surname;
+    private int age;
+
+    public Student(int ID, String name , String surname, int age){
+        this.ID = ID;
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
+        Student sebike = new Student(10,"Sebi","Jakabcin",21);
+        System.out.println(sebike.getAge());
+        sebike.setName("Sebastian");
+        System.out.println(sebike.getName());
+
 
     }
+
 }
