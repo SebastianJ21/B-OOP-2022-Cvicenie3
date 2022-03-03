b1d0bfc79dbac953f27a2cb6e3f944be27dc23c8
Sebastian <xjakabcin@stuba.sk>
2022-03-03 10:01:26 +0100

    Split Student class into its own file

--- src/main/java/sk/stuba/fei/uim/oop/Main.java
+++ src/main/java/sk/stuba/fei/uim/oop/Main.java
@@ -5,52 +5,6 @@ import java.util.Collection;
 import java.util.Comparator;
 import java.util.Locale;
 
-class Student{
-    private int  ID;
-    private String name;
-    private String surname;
-    private int age;
-
-    public Student(int ID, String name , String surname, int age){
-        this.ID = ID;
-        this.name = name;
-        this.surname = surname;
-        this.age = age;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public int getID() {
-        return ID;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public void setID(int ID) {
-        this.ID = ID;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-}
-
 public class Main {
     public static void main(String[] args) {
 
@@ -88,7 +42,6 @@ public class Main {
 
         if (descending){
             Arrays.sort(arr, Comparator.comparing(Student::getName).reversed());
-            return;
 
         } else {
             Arrays.sort(arr, Comparator.comparing(Student::getName));
---
+++ src/main/java/sk/stuba/fei/uim/oop/Student.java
@@ -0,0 +1,52 @@
+package sk.stuba.fei.uim.oop;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.Locale;
+
+class Student{
+    private int  ID;
+    private String name;
+    private String surname;
+    private int age;
+
+    public Student(int ID, String name , String surname, int age){
+        this.ID = ID;
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+}
